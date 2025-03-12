5a4a52f8dafb378ca833679ff66a2e4e6678564d
scadaguru <scadaguru@gmail.com>
2025-03-12 18:01:55 -0500

    Initial update

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # backup-mac-to-pbs
 Do backup of your MAC OS to Proxmox Backup Server
+
+## This repo will allow you to do backup of your MAC OS to Proxmox Backup Server.
+### You will need:
+- Proxmox Backup Server installed and running somewhere in your network
+- You will need a MAC device to backup of it's folder to the Proxmox Backup Server
+- Your MAC must have Docker Desktop offical application installed
+
+### Clone this repo and update follwing files
+- docker-compose.yaml line #13 shown below:
+    Make changes on the left side to point your folder that you want to get backup of, no changes need on the right side.
+    - /Volumes/Data/work/another-python:/backup
+- using terminal CD to that folder run the following command
+    docker compose up -d --force-recreate
+
+### Notes:
+- This will run backup at every hour using crontab but will do only one backup per day, cronjob runs hourly in case if your MAC was sleeping or not on so it will get backuped next hour if it is still on once a day
---
+++ data/custom_cronjobs/backup-mac-to-pbs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Following two will stay same for all VMs
+cur_folder="/home/custom_cronjobs/"
+log_file=$cur_folder/backup.log
+backup_marker=$cur_folder"last-ran-backup"
+
+echo "" # for newline in the log!
+
+# first check if backup was done today/date
+if [[ $(date -r $backup_marker +%Y-%m-%d) == $(date +%Y-%m-%d) ]]; then
+  echo `date +%Y-%m-%d" "%H:%M:%S`": The backup was created earlier for today's date, so ignoring: "
+  exit -1
+fi
+# now create the marker file to prevent the same day backup
+touch $backup_marker;
+
+# run the backup now
+echo `date +%Y-%m-%d" "%H:%M:%S`": About to start the backup"
+
+# update below 3 lines variables for your environment, if you don't know how to then follow Proxmox documentation: https://pbs.proxmox.com/docs/backup-client.html
+export PBS_REPOSITORY=
+export PBS_PASSWORD=
+export PBS_FINGERPRINT=
+
+proxmox-backup-client backup root.pxar:/backup
+# if you want you can specify the NAMESPACE (--ns) and backup-id (--backup-id) if configured on Proxmox Backup Server
+# proxmox-backup-client backup root.pxar:/backup --ns host --backup-id "mac-mini4"
+
+# no changes needed below 3 lines!
+export PBS_REPOSITORY=
+export PBS_PASSWORD=
+export PBS_FINGERPRINT=
+
+echo `date +%Y-%m-%d" "%H:%M:%S`": Finished backup"
+echo "" # for newline in the log!
